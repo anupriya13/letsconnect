9abb0f7cc47d5867bb9d9ceeff4b1ff499fdfd7a
Anupriya Verma <54227869+anupriya13@users.noreply.github.com>
2021-04-27 16:17:23 +0530

    Update Menubar.js

--- client/src/components/Menubar/Menubar.js
+++ client/src/components/Menubar/Menubar.js
@@ -15,7 +15,8 @@ const Navbar = () => {
                 <Typography component={Link} to="/" className={classes.heading} variant = "h6" align = "center"> Posts </Typography>
                 &nbsp;&nbsp; | &nbsp; &nbsp;
                 <Typography>< a target="_blank" href="https://chat-room-web.netlify.app/"> Chat Room </a></Typography>
-               
+                &nbsp;&nbsp; | &nbsp; &nbsp;
+                <Typography>< a target="_blank" href="https://video-connect.netlify.app/"> Video Room </a></Typography>
                
                
 
